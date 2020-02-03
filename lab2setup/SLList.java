da8fd78262020f26e048a40bfe35235a77882320
cool4zbl <i@zhangbinliu.me>
2020-02-03 08:34:57 +0800

    Add `SLList` & `IntNode`

--- lab2setup/SLList.java
+++ lab2setup/SLList.java
@@ -1,2 +1,31 @@
+/**
+ * An SLList is a list of integers.
+ */
+
 public class SLList {
+    public IntNode first;
+
+    public SLList(int x) {
+        first = new IntNode(x, null);
+    }
+
+    /**
+     * Adds x to the front of the list
+     */
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+    }
+
+    public int getFirst() {
+        return first.item;
+    }
+
+    public static void main(String[] args) {
+        SLList L = new SLList(15);
+        L.addFirst(10);
+        L.addFirst(5);
+        int a = L.getFirst();
+        System.out.println(a);
+    }
+
 }
