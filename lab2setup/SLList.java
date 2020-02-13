6b6d4cd7a9d01d7ede4ded76c1ac074f56d230e6
cool4zbl <i@zhangbinliu.me>
2020-02-13 09:19:47 +0800

    update SLList

--- lab2setup/SLList.java
+++ lab2setup/SLList.java
@@ -3,7 +3,18 @@
  */
 
 public class SLList {
+    private static class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+
     private IntNode sentinel;
+    private IntNode last;
 
     private int size;
 
@@ -13,19 +24,16 @@ public class SLList {
         size = 1;
     }
 
+    // TODO:
+//    public SLList(int[] a) {
+//        sentinel = new IntNode(22, null);
+//        sentinel.next = new IntNode(x, null);
+//        size = 1;
+//    }
+
     public SLList() {
         sentinel = new IntNode(22, null);
         size = 0;
-    }
-
-    private static class IntNode {
-        public int item;
-        public IntNode next;
-
-        public IntNode(int i, IntNode n) {
-            item = i;
-            next = n;
-        }
     }
 
     /**
@@ -40,14 +48,26 @@ public class SLList {
         return sentinel.next.item;
     }
 
+    public void deleteFirst() {
+        IntNode p = sentinel.next;
+
+        if (p != null) {
+            size -= 1;
+            sentinel.next = p.next;
+        }
+    }
+
     public void addLast(int x) {
         size += 1;
 
-        IntNode p = sentinel;
-        while (p.next != null) {
-            p = p.next;
-        }
-        p.next = new IntNode(x, null);
+        last.next = new IntNode(x, null);
+        last = last.next;
+
+//        IntNode p = sentinel;
+//        while (p.next != null) {
+//            p = p.next;
+//        }
+//        p.next = new IntNode(x, null);
     }
 
     public int size() {
@@ -65,8 +85,14 @@ public class SLList {
         System.out.println(a);
         System.out.println(size);
 
+        L.deleteFirst();
+        System.out.println(L.size());
+        System.out.println(L.getFirst());
+
         SLList Q = new SLList();
+        Q.deleteFirst();
         Q.addLast(4);
+
         System.out.println(Q.size());
     }
 }
