708516673119074caf8acc95ced9775d7aff1b56
cool4zbl <i@zhangbinliu.me>
2020-02-15 11:04:35 +0800

    feat: use `generics` in SLList

--- lab2setup/SLList.java
+++ lab2setup/SLList.java
@@ -2,54 +2,48 @@
  * An SLList is a list of integers.
  */
 
-public class SLList {
-    private static class IntNode {
-        public int item;
-        public IntNode next;
+public class SLList<Loch> {
+    public class StuffNode {
+        public Loch item;
+        public StuffNode next;
 
-        public IntNode(int i, IntNode n) {
+        public StuffNode(Loch i, StuffNode n) {
             item = i;
             next = n;
         }
     }
 
-    private IntNode sentinel;
-    private IntNode last;
+    private StuffNode sentinel;
+    private StuffNode last;
 
     private int size;
 
-    public SLList(int x) {
-        sentinel = new IntNode(22, null);
-        sentinel.next = new IntNode(x, null);
+    public SLList(Loch x) {
+        sentinel = new StuffNode(x, null);
+        sentinel.next = new StuffNode(x, null);
+        last = sentinel.next;
         size = 1;
     }
 
-    // TODO:
-//    public SLList(int[] a) {
-//        sentinel = new IntNode(22, null);
-//        sentinel.next = new IntNode(x, null);
-//        size = 1;
+//    public SLList() {
+//        sentinel = new StuffNode(2, null);
+//        size = 0;
 //    }
-
-    public SLList() {
-        sentinel = new IntNode(22, null);
-        size = 0;
-    }
 
     /**
      * Adds x to the front of the list
      */
-    public void addFirst(int x) {
+    public void addFirst(Loch x) {
         size += 1;
-        sentinel.next = new IntNode(x, sentinel.next);
+        sentinel.next = new StuffNode(x, sentinel.next);
     }
 
-    public int getFirst() {
+    public Loch getFirst() {
         return sentinel.next.item;
     }
 
     public void deleteFirst() {
-        IntNode p = sentinel.next;
+        StuffNode p = sentinel.next;
 
         if (p != null) {
             size -= 1;
@@ -57,17 +51,11 @@ public class SLList {
         }
     }
 
-    public void addLast(int x) {
+    public void addLast(Loch x) {
         size += 1;
 
-        last.next = new IntNode(x, null);
+        last.next = new StuffNode(x, null);
         last = last.next;
-
-//        IntNode p = sentinel;
-//        while (p.next != null) {
-//            p = p.next;
-//        }
-//        p.next = new IntNode(x, null);
     }
 
     public int size() {
@@ -75,23 +63,22 @@ public class SLList {
     }
 
     public static void main(String[] args) {
-        SLList L = new SLList(15);
+        SLList<Integer> L = new SLList<>(15);
 
         L.addFirst(10);
         L.addFirst(5);
         L.addLast(3);
-        int a = L.getFirst();
         int size = L.size();
-        System.out.println(a);
+        System.out.println(L.getFirst());
         System.out.println(size);
 
         L.deleteFirst();
         System.out.println(L.size());
         System.out.println(L.getFirst());
 
-        SLList Q = new SLList();
+        SLList<String> Q = new SLList<>("hi");
         Q.deleteFirst();
-        Q.addLast(4);
+        Q.addLast("world");
 
         System.out.println(Q.size());
     }
