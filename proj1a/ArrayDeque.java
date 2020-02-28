cb63994933c5948ab2379471ba6a3ceb052187dc
cool4zbl <i@zhangbinliu.me>
2020-02-28 09:39:40 +0800

    fix: ArrayDeque & unittest

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -20,17 +20,16 @@ public class ArrayDeque<T> {
     /**
      * Creates a deep copy of other
      */
-    public ArrayDeque(ArrayDeque other) {
-        for (int i = 0; i < other.size(); i++) {
-            addLast((T) other.get(i));
-        }
-    }
-
-//    private void resize(int capacity) {
-//        T[] a = (T[]) new Object[capacity];
-//        System.arraycopy(items, 0, a, 0, size);
-//        items = a;
+//    public ArrayDeque(ArrayDeque other) {
+//        for (int i = 0; i < other.size(); i++) {
+//            addLast((T) other.get(i));
+//        }
 //    }
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
+    }
 
     /**
      * Adds an item of type `T` to the front of the deque.
@@ -38,6 +37,13 @@ public class ArrayDeque<T> {
      * @param item
      */
     public void addFirst(T item) {
+        items[nextFirst] = item;
+        size++;
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst--;
+        }
     }
 
     /**
@@ -46,6 +52,64 @@ public class ArrayDeque<T> {
      * @param item
      */
     public void addLast(T item) {
+        items[nextLast] = item;
+        size++;
+        if (nextLast == items.length - 1) {
+            nextLast = 0;
+        } else {
+            nextLast++;
+        }
+    }
+
+    /**
+     * Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null.
+     *
+     * @return T
+     */
+    public T removeFirst() {
+        T item = items[nextFirst];
+
+        if (nextFirst == items.length - 1) {
+            nextFirst = 0;
+        } else {
+            nextFirst++;
+        }
+        size--;
+
+        return item;
+    }
+
+    /**
+     * Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null.
+     *
+     * @return T
+     */
+    public T removeLast() {
+        T item = items[nextLast];
+        if (nextLast == 0) {
+            nextLast = items.length - 1;
+        } else {
+            nextLast--;
+        }
+        size--;
+
+        return item;
+    }
+
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null.
+     * Must not alter the deque!
+     * Use `iteration`
+     *
+     * @param index
+     * @return T
+     */
+    public T get(int index) {
+        if (index < 0 || index >= size) return null;
+        return items[index];
     }
 
     /**
@@ -65,54 +129,15 @@ public class ArrayDeque<T> {
     public void printDeque() {
     }
 
-    /**
-     * Removes and returns the item at the front of the deque.
-     * If no such item exists, returns null.
-     *
-     * @return T
-     */
-    public T removeFirst() {
-    }
-
-    /**
-     * Removes and returns the item at the back of the deque.
-     * If no such item exists, returns null.
-     *
-     * @return T
-     */
-    public T removeLast() {
-    }
-
-    /**
-     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
-     * If no such item exists, returns null.
-     * Must not alter the deque!
-     * Use `iteration`
-     *
-     * @param index
-     * @return T
-     */
-    public T get(int index) {
-        if (index < 0 || index >= size) return null;
-        return items[index];
-    }
-
-    private T traverse(T[] arr, int index) {
-
-    }
-
-    /**
-     * Same as get, but uses recursion.
-     *
-     * @param index
-     * @return T
-     */
-    public T getRecursive(int index) {
-        if (index < 0 || index >= size) return null;
-        return traverse(items, index);
-    }
-
     public int size() {
         return size;
+    }
+
+    public int getNextFirst() {
+        return nextFirst;
+    }
+
+    public int getNextLast() {
+        return nextLast;
     }
 }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -21,6 +21,112 @@ public class ArrayDequeTest {
     public void testAddLastAndSize() {
         ArrayDeque<String> L = new ArrayDeque<>();
         L.addLast("a");
+        L.addLast("b");
+        L.addLast("d");
+        L.addLast("e");
+        // "a" -> "b" -> "d" -> "e"
+        assertEquals(4, L.size());
+        assertEquals("e", L.get(L.getNextLast() - 1));
+        // TODO:
+        assertEquals("d", L.get(L.getNextLast() - 2));
+        assertEquals("a", L.get(L.getNextFirst() + 1));
+        assertEquals("b", L.get(L.getNextFirst() + 2));
+    }
+
+    @Test
+    public void testAddFirstAndSize() {
+        ArrayDeque<String> L = new ArrayDeque<>();
+        L.addFirst("e");
+        L.addFirst("d");
+        L.addFirst("b");
+        L.addFirst("a");
+        // "a" -> "b" -> "d" -> "e"
+        assertEquals(4, L.size());
+        assertEquals("e", L.get(L.getNextLast() - 1));
+        // TODO:
+        assertEquals("d", L.get(L.getNextLast() - 2));
+        assertEquals("a", L.get(L.getNextFirst() + 1));
+        assertEquals("b", L.get(L.getNextFirst() + 2));
+    }
+
+    @Test
+    public void testAddAndNextFirstAndLast() {
+        ArrayDeque<String> L = new ArrayDeque<>();
+        L.addLast("a");
+        L.addLast("b");
+        L.addFirst("c");
+        L.addLast("d");
+        L.addLast("e");
+        L.addFirst("f");
+        assertEquals(6, L.size());
+        assertEquals(2, L.getNextFirst());
+        assertEquals(1, L.getNextLast());
+    }
+
+    @Test
+    public void testRemoveLastAndSize() {
+        ArrayDeque<String> L = new ArrayDeque<>();
+        L.addLast("a");
+        L.addFirst("b");
+        L.addLast("c");
+        L.addFirst("d");
+        // "d" -> "b" -> "a" -> "c"
+        String last = L.removeLast();
+        assertEquals(3, L.size());
+        assertEquals(last, "c");
+        String last1 = L.removeLast();
+        assertEquals(2, L.size());
+        assertEquals(last1, "a");
+
+        String last2 = L.removeLast();
         assertEquals(1, L.size());
+        assertEquals(last2, "b");
+
+        String last3 = L.removeLast();
+        assertEquals(0, L.size());
+        assertEquals(last3, "d");
+    }
+
+    @Test
+    public void testRemoveFirstAndSize() {
+        ArrayDeque<String> L = new ArrayDeque<>();
+        L.addLast("a");
+        L.addFirst("b");
+        L.addLast("c");
+        L.addFirst("d");
+        // "d" -> "b" -> "a" -> "c"
+        String first0 = L.removeFirst();
+        assertEquals(3, L.size());
+        assertEquals(first0, "d");
+        String first1 = L.removeFirst();
+        assertEquals(2, L.size());
+        assertEquals(first1, "b");
+
+        String first2 = L.removeFirst();
+        assertEquals(first2, "a");
+        assertEquals(1, L.size());
+
+        String first3 = L.removeFirst();
+        assertEquals(first3, "c");
+        assertEquals(0, L.size());
+    }
+
+    /**
+     * Tests insertion of a large number of items.
+     */
+//    @Test
+//    public void testMegaInsert() {
+//        ArrayDeque<Integer> L = new ArrayDeque<>();
+//        int N = 1000000;
+//        for (int i = 0; i < N; i += 1) {
+//            L.addLast(i);
+//        }
+//
+//        for (int i = 0; i < N; i += 1) {
+//            L.addLast(L.get(i));
+//        }
+//    }
+    public static void main(String[] args) {
+        jh61b.junit.TestRunner.runTests("all", ArrayDequeTest.class);
     }
 }
